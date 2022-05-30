1f9b8e3f6285258fae07ceb32190c9bf306de397
ume <Mayoume123456@gmail.com>
2022-05-30 12:41:37 +0000

    change create-button-word

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -16,7 +16,7 @@
                     {!! Form::text('status', null, ['class' => 'form-control']) !!}
                 </div>
 
-                {!! Form::submit('投稿', ['class' => 'btn btn-primary']) !!}
+                {!! Form::submit('作成', ['class' => 'btn btn-primary']) !!}
 
             {!! Form::close() !!}
         </div>
